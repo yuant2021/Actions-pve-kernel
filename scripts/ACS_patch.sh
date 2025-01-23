2bb9be849ce5c1512296b786555a53a4776381b1
yuant <42244015+yuant2021@users.noreply.github.com>
2025-01-23 14:32:04 +0800

    Update ACS_patch.sh

--- scripts/ACS_patch.sh
+++ scripts/ACS_patch.sh
@@ -1,3 +1,2 @@
-sed -i "s/106 @@/105 @@/g" patches/kernel/0004-pci-Enable-overrides-for-missing-ACS-capabilities-4..patch
-sed -i "/pci_find_ext_capability(dev, PCI_EXT_CAP_ID_ACS))/d" patches/kernel/0004-pci-Enable-overrides-for-missing-ACS-capabilities-4..patch
-sed -i "s/(dev) ||/(dev))/g" patches/kernel/0004-pci-Enable-overrides-for-missing-ACS-capabilities-4..patch
+cd patches/kernel/
+wget https://github.com/coolsnowwolf/lede/raw/refs/heads/master/target/linux/x86/patches-6.12/993-bnx2x_warpcore_8727_2_5g_sgmii_txfault.patch
